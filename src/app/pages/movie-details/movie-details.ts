c8e5eca4e01198272cb440f32b17252d1d5eab71
GianKouvelas <gianniskouv13@gmail.com>
2026-04-24 18:18:19 +0300

    Popup Complete

--- src/app/pages/movie-details/movie-details.ts
+++ src/app/pages/movie-details/movie-details.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Movie } from '../../services/movie';
@@ -8,7 +8,8 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-movie-details',
   imports: [CommonModule, MatDialogModule, FormsModule],
   templateUrl: './movie-details.html',
-  styleUrl: './movie-details.css'
+  styleUrl: './movie-details.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieDetails {
   movie: any = null;
@@ -19,7 +20,8 @@ export class MovieDetails {
   constructor(
     private movieService: Movie,
     public dialogRef: MatDialogRef<MovieDetails>,
-    @Inject(MAT_DIALOG_DATA) public data: { movieId: number }
+    @Inject(MAT_DIALOG_DATA) public data: { movieId: number },
+    private cdr: ChangeDetectorRef
   ) {
     this.loadDetails();
     this.createGuestSession();
@@ -27,21 +29,30 @@ export class MovieDetails {
 
   loadDetails() {
     this.movieService.getMovieDetails(this.data.movieId).subscribe({
-      next: (data) => this.movie = data,
+      next: (data) => {
+        this.movie = data;
+        this.cdr.markForCheck();
+      },
       error: (err) => console.error(err)
     });
   }
 
   createGuestSession() {
     this.movieService.getGuestSession().subscribe({
-      next: (data) => this.guestSessionId = data.guest_session_id,
+      next: (data) => {
+        this.guestSessionId = data.guest_session_id;
+        this.cdr.markForCheck();
+      },
       error: (err) => console.error(err)
     });
   }
 
   submitRating() {
     this.movieService.rateMovie(this.data.movieId, this.rating, this.guestSessionId).subscribe({
-      next: () => this.ratingSubmitted = true,
+      next: () => {
+        this.ratingSubmitted = true;
+        this.cdr.markForCheck();
+      },
       error: (err) => console.error(err)
     });
   }
